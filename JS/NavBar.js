8030a50cf1977b6e824ec6a4157ecf844cce7803
Nicholas Pung <nicholaspung>
2019-05-21 15:41:56 -0700

    changed js folder name to see if github pages stops error

--- JS/NavBar.js
+++ JS/NavBar.js
@@ -1,17 +1,11 @@
+// Hamburger Menu
 let navLinks = document.querySelectorAll('li');
 
 let menu = document.querySelector('.menu');
-
-// function openMenu() {
-//     if (navLinks[1].style.display == 'block') {
-//         navLinks.forEach(e => e.style.display = 'none');
-//     } else {
-//         navLinks.forEach(e => e.style.display = 'block');
-//     }
-// }
 
 function openMenu() {
     navLinks.forEach(e => e.classList.toggle('not-hidden'));
 }
 
 menu.addEventListener('click', openMenu);
+
